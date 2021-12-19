34d762300d8927328f1b537eef0acf4164073ba2
freedomcry <90600100+ZhandosBaltabaev@users.noreply.github.com>
2021-12-19 19:19:26 +0600

    Add files via upload

---
+++ 5.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$a = 2;
+$b = 5;
+if ($a % 2 == 0)
+    echo $b.' - нечетное число';
+else
+    echo $a.' - нечетное число';
+?>
+</body>
+</html>
