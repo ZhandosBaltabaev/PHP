e44f2ecaf5a33ed37c7e1b783032d1a48061b7b1
freedomcry <90600100+ZhandosBaltabaev@users.noreply.github.com>
2021-12-19 19:13:00 +0600

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$x = 4.2;
+$y = 5.6;
+echo $x == 15;
+echo $y >= 6;
+echo $x == $y;
+?>
+</body>
+</html>
+
